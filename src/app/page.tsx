29e6e67565e8f7b6569c0b4acd4596d34fcc67aa
GASPing4ER <simecgasper@gmail.com>
2024-11-18 20:57:16 +0100

    ustvarjena struktura headerja

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className="w-full h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">ZMAGA</h1>
+    </main>
+  );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+// Header Component
+
+const Header = () => {
+  return (
+    <header className="w-full h-20 fixed top-0 left-0 border flex justify-between items-center">
+      <div>
+        {/* TODO: Nakupovalni meni tukaj */}
+        {/* TODO: Ostali linki do strani tukaj */}
+      </div>
+      <h1>LOGO ALNEKI</h1> {/* TODO: Logo tukaj! */}
+      <div>
+        {/* TODO: Izbira jezika tukaj */}
+        {/* TODO: Ikona za uporabnika tukaj */}
+        {/* TODO: Košarica tukaj */}
+      </div>
+    </header>
+  );
+};
+
+export default Header;
