d35a2e808860f84f42a18b84639f293314bfdc14
Alen Šober <sober.alen.prog@gmail.com>
2024-12-15 19:57:04 +0100

    bug fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,5 +6,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/profile", "/login", "/register"],
+  matcher: ["/profile/:path*", "/login", "/register"],
 };
